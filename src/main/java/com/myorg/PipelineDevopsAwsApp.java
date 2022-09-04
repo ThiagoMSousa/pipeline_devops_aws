6087aac51a925514027788515bbd40f74f328161
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2022-09-04 14:41:44 -0300

    Trocado a ordem de criação da Stack do RDS antes da Stack do Serviço

--- src/main/java/com/myorg/PipelineDevopsAwsApp.java
+++ src/main/java/com/myorg/PipelineDevopsAwsApp.java
@@ -15,18 +15,18 @@ public class PipelineDevopsAwsApp {
 
         // Criando o Cluster atrelando a VPC
         ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
-
         // Colocando dependencia da Criação da VPC para depois criar o Cluster
         clusterStack.addDependency(vpcStack);
-
-        // Criando a Stack do Service01Stack
-        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
-        service01Stack.addDependency(clusterStack);
 
         // Criando o RDS
         RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
         rdsStack.addDependency(vpcStack);
 
+
+        // Criando a Stack do Service01Stack
+        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
+        
         app.synth();
     }
 }
